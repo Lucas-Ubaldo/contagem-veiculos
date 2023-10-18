16b63950986cfe1b3ac5b261568a55c623142754
Lucas-Ubaldo <lucasubaldo123@outlook.com>
2023-10-17 23:29:30 -0300

    Testes subtração e dilatação

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import cv2
-import numpy
+import numpy as np
 from time import sleep
 
 #Recebe o vídeo
 captura = cv2.VideoCapture("traffic.mp4")
+subtracao = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+
 
 while True:
-  _, frame = captura.read() #faz a leitura de cada frame do vídeo
+  ret, frame = captura.read() 
   tempo = float(1/60)
-  sleep(tempo) #Necessário para "normalizar" a velocidade do vídeo
+  sleep(tempo) 
+
   escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  
+  vid_subtraido = subtracao.apply(frame)
+  real_part = cv2.bitwise_and(frame, frame, mask=vid_subtraido)
+  vid_sub3 = cv2.cvtColor(vid_subtraido, cv2.COLOR_GRAY2BGR)
+
+  dilat = cv2.dilate(vid_sub3, np.ones((5,5)))
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+  dilatada = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
+  dilatada = cv2.morphologyEx(dilatada, cv2.MORPH_CLOSE, kernel)
+
   cv2.imshow('Video original', frame)
-  cv2.imshow('Video em escala de cinza', escala_cinza)
+  #cv2.imshow('Video em escala de cinza', escala_cinza)
+  cv2.imshow("Video Subtraido", vid_sub3)
+  #cv2.imshow("Filtro dilatação", dilatada)
+
   if cv2.waitKey(1) == ord ('q'):
     break
 captura.release()
