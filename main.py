524c2de2bce11796c886b2b22e02f890afc1539c
Lucas-Ubaldo <lucasubaldo123@outlook.com>
2023-10-25 11:01:47 -0300

    Função para selecionar área de interesse (ROI)

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ subtracao = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 bbox = cv2.selectROI(frame, False)
 (w1, h1, w2, h2) = bbox
 
+#Função para selecionar uma área de interesse
+def getRoi(frame):
+  roi = frame[h1:h1 + h2, w1:w1 + w2]
+  return roi
+
 def main():
   tempo = float(1/60)
   
@@ -16,16 +21,16 @@ def main():
     ret, frame = captura.read() 
     sleep(tempo) 
 
-    roi = frame[h1:h1 + h2, w1:w1 + w2]
-    
     escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    roi = getRoi(frame)
     
-    vid_subtraido = subtracao.apply(roi)
+    vid_subtraido = subtracao.apply(escala_cinza)
 
-    cv2.imshow('Video original', frame)
-    cv2.imshow("video", vid_subtraido)
+    cv2.imshow('Video original', vid_subtraido)
+    cv2.imshow("ROI", roi)
 
-    if cv2.waitKey(1) == ord ('q'):
+    if cv2.waitKey(1) == ord ('q'): #Pressionar q para fechar
       break
   captura.release()
   cv2.destroyAllWindows()
