84f031a57fc604eed57eea708ad7292fd83c616a
Lucas-Ubaldo <lucasubaldo123@outlook.com>
2023-11-02 12:34:37 -0300

    Implementado função para aplicação de filtros

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ from time import sleep
 
 captura = cv2.VideoCapture("videos/highway2.mp4")
 hasFrame, frame = captura.read()
-subtracao = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+subtracao = cv2.createBackgroundSubtractorMOG2(history=50, detectShadows=False, varThreshold=200)
 
+def getKernel(tipo_kernel):
+    if tipo_kernel == "dilation":
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+    if tipo_kernel == "opening":
+        kernel = np.ones((5, 5), np.uint8)
+    if tipo_kernel == "closing":
+        kernel = np.ones((10, 10), np.uint8)
+    return kernel
+
+def getFilter(subtraido):
+    closing = cv2.morphologyEx(subtraido, cv2.MORPH_CLOSE, getKernel("closing"), iterations=2)
+    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, getKernel("opening"), iterations=2)
+    dilation = cv2.dilate(opening, getKernel("dilation"), iterations=2)
+    return dilation
+
+#ROI
 bbox = cv2.selectROI(frame, False)
 (w1, h1, w2, h2) = bbox
 
@@ -18,16 +34,19 @@ def main():
   tempo = float(1/60)
   
   while True:
-    ret, frame = captura.read() 
+    ret, frame = captura.read()
+    if frame is None:
+      break 
     sleep(tempo) 
 
     escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     roi = getRoi(frame)
     
-    vid_subtraido = subtracao.apply(escala_cinza)
+    subtraido = subtracao.apply(roi)
+    dilatado = getFilter(subtraido)
 
-    cv2.imshow('Video original', vid_subtraido)
+    cv2.imshow('Video original', dilatado)
     cv2.imshow("ROI", roi)
 
     if cv2.waitKey(1) == ord ('q'): #Pressionar q para fechar
