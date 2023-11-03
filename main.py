9b06ffce47d6053e800884c3e3b138c9bc8bb5f2
Lucas-Ubaldo <lucasubaldo123@outlook.com>
2023-11-03 12:28:18 -0300

    Adicionado linhas de delimitação para contagem

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ from time import sleep
 
 captura = cv2.VideoCapture("videos/highway2.mp4")
 hasFrame, frame = captura.read()
+
 subtracao = cv2.createBackgroundSubtractorMOG2(history=50, detectShadows=False, varThreshold=200)
+
+#ROI
+bbox = cv2.selectROI(frame, False)
+(w1, h1, w2, h2) = bbox
+
+#Linhas em relação ao ROI
+linha1 = int(h1)
+linha2 = int(h2 - 20)
+linhas_color = (255, 255, 255)
+
 
 def getKernel(tipo_kernel):
     if tipo_kernel == "dilation":
@@ -21,38 +32,35 @@ def getFilter(subtraido):
     dilation = cv2.dilate(opening, getKernel("dilation"), iterations=2)
     return dilation
 
-#ROI
-bbox = cv2.selectROI(frame, False)
-(w1, h1, w2, h2) = bbox
-
-#Função para selecionar uma área de interesse
 def getRoi(frame):
   roi = frame[h1:h1 + h2, w1:w1 + w2]
   return roi
 
-def main():
+while True:
+  ret, frame = captura.read()
   tempo = float(1/60)
-  
-  while True:
-    ret, frame = captura.read()
-    if frame is None:
-      break 
-    sleep(tempo) 
+  if frame is None:
+    break 
+  sleep(tempo) 
 
-    escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    roi = getRoi(frame)
+  roi = getRoi(frame)
     
-    subtraido = subtracao.apply(roi)
-    dilatado = getFilter(subtraido)
+  subtraido = subtracao.apply(roi)
+  dilatado = getFilter(subtraido)
 
-    cv2.imshow('Video original', dilatado)
-    cv2.imshow("ROI", roi)
+  cv2.imshow('Video c/ filtros', dilatado)
+  cv2.imshow("ROI", roi)
 
-    if cv2.waitKey(1) == ord ('q'): #Pressionar q para fechar
-      break
-  captura.release()
-  cv2.destroyAllWindows()
-main()
+  frame = cv2.line(frame, (w1, linha1), (w1 + w2, linha1), linhas_color, 2)
+  frame = cv2.line(frame, (w1, h1 + linha2), (w1 + w2, h1 + linha2), linhas_color, 2)
+  cv2.imshow("Frame", frame)
+
+  if cv2.waitKey(1) == ord ('q'): #Pressionar q para fechar
+    break
+captura.release()
+cv2.destroyAllWindows()
+
 
 
