b1da741ade0811533b3205d8b5066eeff4eadf01
Lucas-Ubaldo <lucasubaldo123@outlook.com>
2023-11-07 12:35:49 -0300

    Incrementado função para detectar veículos e encontrar centroid

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ linha2 = int(h2 - 20)
 linhas_color = (255, 255, 255)
 
 
-def getKernel(tipo_kernel):
+def gerar_kernel(tipo_kernel):
     if tipo_kernel == "dilation":
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
     if tipo_kernel == "opening":
@@ -26,15 +26,27 @@ def getKernel(tipo_kernel):
         kernel = np.ones((10, 10), np.uint8)
     return kernel
 
-def getFilter(subtraido):
-    closing = cv2.morphologyEx(subtraido, cv2.MORPH_CLOSE, getKernel("closing"), iterations=2)
-    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, getKernel("opening"), iterations=2)
-    dilation = cv2.dilate(opening, getKernel("dilation"), iterations=2)
+def aplicar_filtro(subtraido):
+    closing = cv2.morphologyEx(subtraido, cv2.MORPH_CLOSE, gerar_kernel("closing"), iterations=2)
+    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, gerar_kernel("opening"), iterations=2)
+    dilation = cv2.dilate(opening, gerar_kernel("dilation"), iterations=2)
     return dilation
 
-def getRoi(frame):
+def selecionar_area(frame):
   roi = frame[h1:h1 + h2, w1:w1 + w2]
   return roi
+
+def detectar_veiculo(dilatado):
+    contours, _ = cv2.findContours(dilatado, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x + w1, y + h1), (x + w1 + w, y + h1 + h), (255, 255, 0), 2)
+
+            centro_x = x + w1 + w // 2
+            centro_y = y + h1 + h // 2
+            cv2.circle(frame, (centro_x, centro_y), 2, (0, 255, 255), -1)
 
 while True:
   ret, frame = captura.read()
@@ -45,16 +57,19 @@ while True:
 
   escala_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-  roi = getRoi(frame)
+  roi = selecionar_area(frame)
     
   subtraido = subtracao.apply(roi)
-  dilatado = getFilter(subtraido)
+  dilatado = aplicar_filtro(subtraido)
 
   cv2.imshow('Video c/ filtros', dilatado)
   cv2.imshow("ROI", roi)
 
   frame = cv2.line(frame, (w1, linha1), (w1 + w2, linha1), linhas_color, 2)
   frame = cv2.line(frame, (w1, h1 + linha2), (w1 + w2, h1 + linha2), linhas_color, 2)
+
+  deteccao = detectar_veiculo(dilatado)
+
   cv2.imshow("Frame", frame)
 
   if cv2.waitKey(1) == ord ('q'): #Pressionar q para fechar
