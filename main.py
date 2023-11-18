1864754eee8efb4202adbfa3dc852d33f8bf3dd1
Lucas-Ubaldo <lucasubaldo123@outlook.com>
2023-11-18 13:34:28 -0300

    Contagem de veículos

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from time import sleep
 
 class SubtrairFundo:
     def __init__(self):
-        self.subtracao = cv2.createBackgroundSubtractorMOG2(history=25, detectShadows=False, varThreshold=100)
+        self.subtracao = cv2.createBackgroundSubtractorMOG2(history=60, detectShadows=False, varThreshold=100)
 
-    def aplicar_subtracao(self, frame):
-        return self.subtracao.apply(frame)
+    def aplicar_subtracao(self, roi_blur):
+        return self.subtracao.apply(roi_blur)
 
 class SelecionarRegiaoInteresse:
     def __init__(self, frame):
@@ -18,8 +18,10 @@ class SelecionarRegiaoInteresse:
         return frame[self.h1:self.h1 + self.h2, self.w1:self.w1 + self.w2]
 
     def desenhar_linhas(self, frame):
-        cv2.line(frame, (self.w1, self.h1), (self.w1 + self.w2, self.h1), (255, 255, 255), 2)
-        cv2.line(frame, (self.w1, self.h1 + self.h2), (self.w1 + self.w2, self.h1 + self.h2), (255, 255, 255), 2)
+        centro_x = self.w1 + self.w2 // 2
+        centro_y = self.h1 + self.h2 // 2
+        cv2.line(frame, (self.w1, centro_y), (self.w1 + self.w2, centro_y), (255, 255, 255), 2)
+        
 
 class ContadorVeiculos:
     def __init__(self, pos_linha, offset):
@@ -29,12 +31,12 @@ class ContadorVeiculos:
         self.texto_contagem = "Carros detectados: 0"
         self.posicoes_anteriores = set()
 
-    def contar_veiculos(self, centro_y):
-        if (self.pos_linha - self.offset) < centro_y < (self.pos_linha + self.offset):
-            if centro_y not in self.posicoes_anteriores:
+    def contar_veiculos(self, detec):
+        for(x, y) in detec:
+            if (self.pos_linha + self.offset) > y > (self.pos_linha - self.offset):
                 self.carros += 1
-                self.texto_contagem = "Carros detectados: " + str(self.carros)
-                self.posicoes_anteriores.add(centro_y)
+                detec.remove((x, y))
+                self.texto_contagem = "Carros detectados:" + str(self.carros)
 
 class DetectarVeiculo:
     def __init__(self, w1, h1, w2, contador_veiculos):
@@ -42,25 +44,26 @@ class DetectarVeiculo:
         self.h1 = h1
         self.w2 = w2
         self.contador_veiculos = contador_veiculos
+        self.detec = []
 
     def detectar_veiculo(self, frame, dilatado):
-        contours, _ = cv2.findContours(dilatado, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > 500:
-                x, y, w, h = cv2.boundingRect(cnt)
-
-                cv2.rectangle(frame, (x + self.w1, y + self.h1), (x + self.w1 + w, y + self.h1 + h), (0, 255, 0), 2)
-                centro_x = x + self.w1 + w // 2
-                centro_y = y + self.h1 + h // 2
-                cv2.circle(frame, (centro_x, centro_y), 2, (0, 255, 255), -1)
-
-                self.contador_veiculos.contar_veiculos(centro_y)
+        contours, _ = cv2.findContours(dilatado, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for (i, c) in enumerate(contours):
+            (x, y, w, h) = cv2.boundingRect(c)
+            validar_contorno = (w >= 100) and (h >= 100)
+            if not validar_contorno:
+                continue
+            cv2.rectangle(frame, (x + self.w1, y + self.h1), (x + self.w1 + w, y + self.h1 + h), (0, 255, 0), 2)
+            centro_x = x + self.w1 + w // 2
+            centro_y = y + self.h1 + h // 2
+            centro = centro_x, centro_y
+            self.detec.append(centro)
+            cv2.circle(frame, (centro_x, centro_y), 2, (0, 255, 255), -1)
+            self.contador_veiculos.contar_veiculos(self.detec)
 
         
         cv2.putText(frame, self.contador_veiculos.texto_contagem, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-
+        
 class Video:
     def __init__(self, caminho_video):
         self.captura = cv2.VideoCapture(caminho_video)
@@ -75,23 +78,24 @@ class Video:
         while True:
             ret, self.frame = self.captura.read()
             tempo = float(1/60)
+            sleep(tempo)
             if self.frame is None:
                 break
-            sleep(tempo)
 
             roi = self.selecao_roi.selecionar_area(self.frame)
             roi_tons_cinza = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            roi_blur = cv2.GaussianBlur(roi_tons_cinza, (5,5), 5)
 
-            subtraido = self.subtracao_fundo.aplicar_subtracao(roi_tons_cinza)
+            subtraido = self.subtracao_fundo.aplicar_subtracao(roi_blur)
+            
             dilatado = self.aplicar_filtros(subtraido)
-
-            cv2.imshow('Video c/ filtros', dilatado)
-            cv2.imshow("ROI", roi)
 
             self.selecao_roi.desenhar_linhas(self.frame)
 
             self.deteccao_veiculo.detectar_veiculo(self.frame, dilatado)
 
+            cv2.imshow('Video c/ filtros', dilatado)
+            cv2.imshow("ROI", roi)
             cv2.imshow("Frame", self.frame)
 
             if cv2.waitKey(1) == ord('q'):  # Pressionar q para fechar
